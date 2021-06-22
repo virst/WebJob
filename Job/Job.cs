6fce959e9c47d42f0d0e1e25707bc7c7b604d64f
Вирстюк Михаил <m.vi@live.com>
2021-06-22 09:01:41 +0500

    Initial commit

---
+++ Job/Job.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebJob.Job
+{
+    public static class Job
+    {
+        public static readonly Dictionary<string, DateTime> LastRuns = new Dictionary<string, DateTime>();
+        public static readonly TimeSpan Period = new TimeSpan(0, 0, 30);
+        public const int CoolDown = 5 * 1000; // 5 sec.
+
+        static Job()
+        {
+            LastRuns["JOB - 1"] = DateTime.MinValue;
+            LastRuns["JOB - 2"] = DateTime.MinValue;
+            LastRuns["JOB - 3"] = DateTime.MinValue;
+        }
+
+        static void Run(string s)
+        {
+            Console.WriteLine($"RUN - {s}");
+            File.WriteAllText(s.Replace(' ', '_') + ".log", DateTime.Now.ToLongTimeString());
+            Console.WriteLine($"END - {s}");
+        }
+
+        public static void Loop()
+        {
+            while (true)
+            {
+                try
+                {
+                    foreach (var run in LastRuns)
+                    {
+                        if(run.Value.Add(Period) > DateTime.Now) continue;
+                        Run(run.Key);
+                        LastRuns[run.Key] = DateTime.Now;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("LOP ERR:" + ex);
+                }
+
+                Thread.Sleep(CoolDown);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebJob
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Run jobs");
+            new Thread(Job.Job.Loop).Start();
+            Console.WriteLine("Run web");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
